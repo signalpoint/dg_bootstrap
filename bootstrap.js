214b4e078267af56a3a20c1a0d0be75370600823
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-26 13:39:07 -0500

    Style main menu.

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,34 @@
 dg.modules.bootstrap = new dg.Module();
 
 /**
+ * Implements hook_block_view_alter().
+ */
+function bootstrap_block_view_alter(element, block) {
+
+  switch (block.get('id')) {
+
+    // Make the main menu into a navbar.
+    case 'main_menu':
+      var header =
+          '<div class="navbar-header">' +
+          '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">' +
+          '<span class="sr-only">Toggle navigation</span>' +
+          '<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>' +
+          '</button>' +
+          '<a class="navbar-brand" href="#">' + dg.config('title') + '</a>' +
+          '</div>';
+      element.header = {
+        _markup: header
+      };
+      element.menu._weight = 1;
+      element.menu._attributes['class'].push('nav', 'navbar-nav');
+      break;
+
+  }
+
+}
+
+/**
  * Implements hook_form_alter().
  */
 function bootstrap_form_alter(form, form_state, form_id) {
