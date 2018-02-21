f726a31535b30290c2408ab271ca03b08b0079e7
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-02-21 12:26:02 -0500

    Support text within a font awesome icon.

--- src/widgets/widget.i.js
+++ src/widgets/widget.i.js
@@ -1,22 +1,27 @@
 /**
  * Themes a font awesome icon.
  * @param variables
+ *  "_text" {String} Optional, text to go inside the icon element.
  * @returns {string}
  */
 dg.theme_i = function(variables) {
-  return '<i ' + dg.attrs(variables) + '></i>';
+  var text = variables._text ? variables._text : '';
+  return '<i ' + dg.attrs(variables) + '>' + text + '</i>';
 };
 
 /**
  * A shortcut to theme a font awesome icon.
- * @param iconClass
+ * @param iconClass {String} The font awesome class name, excluding the "fa-".
+ * @param text {String} Optional, text to go inside the icon element.
  * @returns {*}
  */
-dg.i = function(iconClass) {
-  return dg.theme('i', {
+dg.i = function(iconClass, text) {
+  var iconVars = {
     _attributes: {
       class: ['fa fa-' + iconClass],
       'aria-hidden': 'true'
     }
-  });
+  };
+  if (arguments[1]) { iconVars._text = arguments[1]; }
+  return dg.theme('i', iconVars);
 };
