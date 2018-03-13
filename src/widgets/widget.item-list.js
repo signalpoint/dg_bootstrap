8aa60a78801c032ef839789a29db99cf81ba95c0
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-03-13 16:41:48 -0400

    Abstract out prepListItem() from bootstrap_item_list(). Simplify 'list-group' assignment.

--- src/widgets/widget.item-list.js
+++ src/widgets/widget.item-list.js
@@ -1,22 +1,21 @@
 dg.theme_bootstrap_item_list = function(variables) {
-  if (!jDrupal.inArray('list-group', variables._attributes.class)) {
-    variables._attributes.class.push('list-group');
-  }
+  var className = 'list-group';
+  var classNames = variables._attributes.class;
+  if (!dg.inArray(className, classNames)) { classNames.push('list-group'); }
   var items = variables._items ? variables._items : null;
   if (items) {
     for (var i = 0; i < items.length; i++) {
-      var item = items[i];
-      if (typeof item === 'string') { item = { _text: item }; }
-      if (!item._attributes) { item._attributes = {}; }
-      if (!item._attributes.class) { item._attributes.class = []; }
-      else if (typeof item._attributes.class === 'string') {
-        item._attributes.class = [item._attributes.class];
-      }
-      if (!jDrupal.inArray('list-group-item', item._attributes.class)) {
-        item._attributes.class.push('list-group-item');
-      }
-      variables._items[i] = item;
+      variables._items[i] = dg_bootstrap.prepListItem(items[i]);
     }
   }
   return dg.theme('item_list', variables);
+};
+
+dg_bootstrap.prepListItem = function(item) {
+  if (typeof item === 'string') { item = { _text: item }; }
+  dg.attributesInit(item);
+  var className = 'list-group-item';
+  var classNames = item._attributes.class;
+  if (!dg.inArray(className, classNames)) { classNames.push(className); }
+  return item;
 };
