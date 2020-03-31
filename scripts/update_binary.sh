9371ff5e0d8ef233d838ceaf4307309785531ade
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2020-03-30 20:56:18 -0400

    Update update_binary.sh

--- scripts/update_binary.sh
+++ scripts/update_binary.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 git add dg_bootstrap.min.js
-git commit -m "Update dg_bootstrap.min.js binary."
-git push origin 8.x-1.x
+git commit -m "Update dg_bootstrap.min.js"
+git push origin 8.x-2.x
 
