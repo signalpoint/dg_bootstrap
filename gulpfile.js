3b45085aa623bb5b4a7563241d8ba75102d7f672
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2021-02-06 21:39:35 -0500

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
     gp_rename = require('gulp-rename'),
     gp_uglify = require('gulp-uglify');
 
-var dg_bootstrapModuleSrc = [
+var moduleSrc = [
   './src/_dg_bootstrap.js',
   './src/blocks/blocks.*.js',
   './src/forms/form.*.js',
@@ -13,17 +13,23 @@ var dg_bootstrapModuleSrc = [
   './src/widgets/widget.*.js'
 ];
 
-// Task to build the cw_app.min.js file.
-gulp.task('minifyJS', function(){
-  return gulp.src(dg_bootstrapModuleSrc)
-      .pipe(gp_concat('concat.js'))
-      .pipe(gulp.dest(''))
-      .pipe(gp_rename('dg_bootstrap.min.js'))
-      .pipe(gp_uglify())
-      .pipe(gulp.dest(''));
-});
+// Minify JavaScript
+function minifyJs() {
+  console.log('compressing dg_bootstrap.js...');
+  var moduleJs = gulp.src(moduleSrc)
+    .pipe(gp_concat('dg_bootstrap.js'))
+    .pipe(gulp.dest('./'));
+    console.log('compressing dg_bootstrap.min.js...');
+  return moduleJs.pipe(gp_rename('dg_bootstrap.min.js'))
+    .pipe(gp_uglify())
+    .pipe(gulp.dest('./'));
+}
+gulp.task(minifyJs);
 
-gulp.task('default', function () {
-  watch(dg_bootstrapModuleSrc, function(event) { gulp.start('minifyJS') });
-  gulp.start('minifyJS');
+gulp.task('default', function(done) {
+
+  gulp.watch(moduleSrc, gulp.series('minifyJs'));
+
+  done();
+
 });
